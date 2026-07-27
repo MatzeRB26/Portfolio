9d6b1d6673d6ea6bd3573fdafd20ceb4a88a43e5
MatzeRB26 <m.stojak14@gmail.com>
2026-07-27 15:26:37 +0200

    feat: Added the hero section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar],
+  imports: [Navbar, Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
