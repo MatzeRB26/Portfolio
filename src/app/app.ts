cb07fef18f9bee0ed5498e2ffb640915054b5806
MatzeRB26 <m.stojak14@gmail.com>
2026-07-29 13:46:49 +0200

    feat: Inserted the "About Me" section.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Hero],
+  imports: [Navbar, Hero, About],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
