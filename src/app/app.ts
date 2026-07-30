eded5d55e4590311cb74db8b75d0bd383550f657
MatzeRB26 <m.stojak14@gmail.com>
2026-07-30 17:28:58 +0200

    feat: Portfolio section included and adjusted

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
 import { About } from './components/about/about';
 import { Skills } from './components/skills/skills';
+import { Portfolio } from './components/projects/portfolio';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Hero, About, Skills],
+  imports: [Navbar, Hero, About, Skills, Portfolio],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
