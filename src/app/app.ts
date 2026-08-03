936602de9f57b7b30cf35687b49df59ef5b4818d
MatzeRB26 <m.stojak14@gmail.com>
2026-08-03 14:40:33 +0200

    fix: i fixed a icon on the reference section

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,11 +5,12 @@ import { About } from './components/about/about';
 import { Skills } from './components/skills/skills';
 import { Portfolio } from './components/projects/portfolio';
 import { ReferencesComponent } from './components/references/references';
+import { Contact } from './components/contact/contact';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Hero, About, Skills, Portfolio, ReferencesComponent],
+  imports: [Navbar, Hero, About, Skills, Portfolio, ReferencesComponent, Contact],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
