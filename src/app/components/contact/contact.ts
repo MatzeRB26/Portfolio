aa486afcbd12f98a56044328d57a4121efcf791c
MatzeRB26 <m.stojak14@gmail.com>
2026-08-05 16:35:29 +0200

    feat: I've added the validators—meaning users have to enter their name and email.

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,12 +1,44 @@
+// import { Component } from '@angular/core';
+// import { RouterLink } from '@angular/router';
+
+
+// @Component({
+//   selector: 'app-contact',
+//   imports: [RouterLink],
+//   templateUrl: './contact.html',
+//   styleUrl: './contact.scss',
+// })
+// export class Contact {}
+
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
-
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
-export class Contact {}
+export class Contact {
+
+  contactForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      privacy: [false, Validators.requiredTrue],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
+}
