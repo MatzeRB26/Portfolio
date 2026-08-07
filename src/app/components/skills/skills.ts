9146fe2e215dc486e8a243a9d1fe8f4342634a59
MatzeRB26 <m.stojak14@gmail.com>
2026-08-07 15:18:29 +0200

    uploaded via FileZilla

--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -10,31 +10,31 @@ export class Skills {
   skillIcons = [
   {
     name: 'HTML',
-    image:'/assets/icons/html.svg'
+    image:'assets/icons/html.svg'
   },
   {
     name: 'CSS',
-    image:'/assets/icons/css.svg'
+    image:'assets/icons/css.svg'
   },
   {
     name: 'JavaScript',
-    image:'/assets/icons/javascript.svg'
+    image:'assets/icons/javascript.svg'
   },
   {
     name: 'TypeScript',
-    image:'/assets/icons/typescript.svg'
+    image:'assets/icons/typescript.svg'
   },
   {
     name: 'Angular',
-    image:'/assets/icons/angular.svg'
+    image:'assets/icons/angular.svg'
   },
   {
     name: 'Git',
-    image:'/assets/icons/git.svg'
+    image:'assets/icons/git.svg'
   },
   {
     name: 'Rest-Api',
-    image:'/assets/icons/api.svg'
+    image:'assets/icons/api.svg'
   }
 ];
 
