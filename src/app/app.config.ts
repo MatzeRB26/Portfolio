05b430716aa739a196a74a15cca4fd60f79b5324
MatzeRB26 <m.stojak14@gmail.com>
2026-09-23 15:30:30 +0200

    fix: Texts adjusted in the skills section

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,3 @@
-// import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-// import { provideRouter, withInMemoryScrolling } from '@angular/router';
-// import { routes } from './app.routes';
-// import { provideClientHydration } from '@angular/platform-browser';
-// import { provideHttpClient } from '@angular/common/http';
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideBrowserGlobalErrorListeners(),
-//     provideRouter(routes,
-//     withInMemoryScrolling({ scrollPositionRestoration: 'top',})),
-//     provideClientHydration(),
-//     provideHttpClient() 
-//   ]
-// };
-
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
